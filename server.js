e746d658fe85d5f64d37459d9fe16e0383b08542
Eric <esbev@hotmail.com>
2023-07-31 22:40:22 -0500

    some changes and deployed to heroku

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(require(path.join(__dirname, 'routes/notes.js')))
 
-app.get('/notes', (req, res) => {
+app.use( (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/notes', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen( PORT, () => {
   console.log(`Server running...`);
-}).catch((err) => {
-  console.log(err);
+// }).catch((err) => {
+//   console.log(err);
 });
