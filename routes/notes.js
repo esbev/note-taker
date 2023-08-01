125106e6d67fd4b9744cd66833c35c29a62a4e92
Eric <esbev@hotmail.com>
2023-07-31 23:35:38 -0500

    still working on bugs

--- routes/notes.js
+++ routes/notes.js
@@ -1,37 +1,33 @@
 const express = require("express");
-const fs = require("fs");
 const notesRouter = require('express').Router();
+const fs = require("fs");
 const path = require('path');
 const uniqid = require('uniqid');
 let bodyParser = require('body-parser');
-
-const dataPath = path.join(__dirname, "../db/db.json");
 
 let jsonParser = bodyParser.json();
 
 const saveNotes = (data) => {
     const stringData = JSON.stringify(data);
-    fs.writeFileSync(dataPath, stringData);
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), stringData);
 };
 
 const getNotes = () => {
-    const jsonData = fs.readFileSync(dataPath);
+    const jsonData = fs.readFileSync(path.join(__dirname, "../db/db.json"));
     return JSON.parse(jsonData);
 };
 
-notesRouter.get = ('/api/notes', (req, res) => {
+notesRouter.get = ('/api/notes.html', (req, res) => {
     const notes = getNotes();
     res.send(notes);
 });
 
-notesRouter.post = ('/api/notes', jsonParser, (req, res) => {
+notesRouter.post = ('/api/notes.html', jsonParser, (req, res) => {
     let savedNotes = getNotes();
     req.body.id = uniqid();
     savedNotes.push(req.body);
     saveNotes(savedNotes);
-    res.send("Note has not been added!");
+    res.send("Note has been added!");
 });
-
-notesRouter.delete = () => {};
 
 module.exports = notesRouter;
--- server.js
+++ server.js
@@ -4,16 +4,15 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')))
-// app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(require(path.join(__dirname, 'routes/notes.js')))
 
-app.use( (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+app.use( (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.listen( PORT, () => {
